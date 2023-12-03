13be6c9fb9a946c884f9ac050168549cf2ec4edb
pudgekaramelkin <whowiewas@gmail.com>
2023-12-03 23:09:30 +0300

    final ver so early???

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardItem) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardItem.link;
+  cardElement.querySelector('.card__image').alt = cardItem.name;
+  cardElement.querySelector('.card__title').textContent = cardItem.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function(event) {
+    event.target.closest('.card').remove();
+  });
+  cardsList.append(cardElement);
+}
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach(cardItem => {
+  createCard(cardItem);
+});
